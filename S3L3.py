96decbbbda96e7f44d20dedf2a2d4ce1a2fcd48c
Maurizio-Pietrangeli <161327196+Maurizio-Pietrangeli@users.noreply.github.com>
2024-03-13 11:35:02 -0400

    Add files via upload

---
+++ S3L3.py
@@ -0,0 +1,62 @@
+print ("calcolare il perimetro di un quadrato")
+print ("inserisci il valore del lato")
+
+lato=int(input())
+
+print ("il perimetro del quadrato è ", lato*4)
+
+
+
+print ("calcolare l'area di un quadrato")
+print ("inserisci il valore del lato")
+
+lato=int(input())
+
+print ("l'area del quadrato è" , lato**2)
+
+
+
+
+print ("calcolare il perimetro di un rettangolo")
+print ("inserisci il valore dell'altezza")
+
+altezza=int(input())
+
+print ("inserisci il valore della base")
+
+base=int(input())
+
+print ("il perimetro del rettangolo è ", altezza*2 + base*2 )
+
+print ("calcolare l'area del rettangolo")
+print ("inserisci il valore dell'altezza")
+
+altezza=int(input())
+
+print ("inserisci il valore della base")
+
+base=int(input())
+
+print ("l'area del rettangolo è" , base*altezza)
+
+
+
+
+
+print ("calcolare la circongerenza di un cerchio")
+print ("inserisci il valore del raggio")
+
+raggio=float(input())
+
+print ("la circonferenza del cerchio è " , 2 * raggio * 3.14)
+
+
+print ("calcolare l'area di un cerchio")
+print ("inserisci il valore del raggio")
+
+raggio=float(input())
+
+print ("l'area del cerchio è" , raggio**2*3.14)
+
+
+
